53c4b994904015f464b371c5b8fcde751c235cc9
niveditha <niveditha.k@crestaerospace.com>
2026-02-24 09:47:50 +0530

    Landing page

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Button } from '../ui/button';
 
 function Header() {
@@ -6,12 +6,12 @@ function Header() {
         <>
             <div className='w-full py-4 px-2 shadow-sm flex justify-between items-center'>
                 <div className='flex items-center gap-2'>
-                    <img src="/logoipsum-295.svg" alt="logo" className='w-10 h-10' />
-                    <h2>AI Travel Planner</h2>
+                    <img src="/logoipsum-295.svg" alt="logo" className='w-5 h-5' />
+                    <h2 className='text-xl font-bold'>AI Travel Planner</h2>
                 </div>
                 <div>
-                    <Button variant="ghost">Sign In</Button>
-                    <Button variant="ghost">Sign Up</Button>
+                    <Button variant="ghost" className='text-sm'>Sign In</Button>
+                    <Button variant="ghost" className='text-sm'>Sign Up</Button>
                 </div>
             </div>
         </>
--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Button } from '../ui/button'
+
+import { Link } from 'react-router-dom'
 
 function Hero() {
     return (
         <>
-            <div className='flex items-center mx-56 gap-9'>
+            <div className='flex flex-col items-center mx-56 gap-9'>
                 <div>
-                    <h1 className='font-extra text-[60px] text-center'><span className='text-[#ff6464]'>Discover Your Next Adventure with AI:</span>Personalized Travel Plans at Your Fingertips</h1>
+                    <h1 className='font-extrabold text-[60px] text-center mt-16'><span className='text-[#ff6464d2]'>Discover Your Next Adventure with AI:</span>Personalized Travel Plans at Your Fingertips</h1>
+                    <p className='text-xl text-gray-500 text-center mt-4'>Your personal trip planner and travel curator, creating custom iternities tailored to your interests and budget</p>
                 </div>
+                <Link to={'/create-trip'}>
+                    <Button className='bg-[#000000] text-[#ffffff] py-3 px-6 rounded-lg font-bold text-lg hover:bg-[#6c6464] transition-all'>
+                        Get Started, It's Free!
+                    </Button>
+                </Link>
             </div>
         </>
     )
