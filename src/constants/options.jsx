4f1a877b010ba4feef111b54c64170e040dbd1d8
Niveditha02 <niveditha.mpn@gmail.com>
2026-03-11 11:05:05 -0400

    constant options.jsx

---
+++ src/constants/options.jsx
@@ -0,0 +1,55 @@
+export const TravelOptions = [
+    {
+        id: 1,
+        title: "Just Me",
+        desc: "A solo traveler exploring the world",
+        icon: "🧍",
+        people: "1 Person"
+    },
+    {
+        id: 2,
+        title: "Couple",
+        desc: "Perfect trip for two people",
+        icon: "👫",
+        people: "2 People"
+    },
+    {
+        id: 3,
+        title: "Family",
+        desc: "Fun trip with family members",
+        icon: "👨‍👩‍👧‍👦",
+        people: "3 to 5 People"
+    },
+    {
+        id: 4,
+        title: "Friends",
+        desc: "Adventure trip with friends",
+        icon: "🧑‍🤝‍🧑",
+        people: "5 to 10 People"
+    }
+];
+
+export const BudgetOptions = [
+    {
+        id: 1,
+        title: "Cheap",
+        desc: "Stay conscious of costs",
+        icon: "💸"
+    },
+    {
+        id: 2,
+        title: "Moderate",
+        desc: "Keep cost on the average side",
+        icon: "💰"
+    },
+    {
+        id: 3,
+        title: "Luxury",
+        desc: "Don't worry about cost",
+        icon: "💎"
+    }
+];
+
+// export const AI_PROMPT = 'Generate travel plan for Location : {Location}';
+
+
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,17 +1,113 @@
-import React from 'react'
+
+// Geoapify Geocoder Autocomplete
+import React, { useState, useEffect } from 'react'
+import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete';
+import '@geoapify/geocoder-autocomplete/styles/minimal.css';
+import { Input } from '@/components/ui/input';
+import { BudgetOptions, TravelOptions } from '@/constants/options';
+import { Button } from '@/components/ui/button';
 
 function CreateTrip() {
+    const [place, setPlace] = useState();
+    const [formData, setFormData] = useState({});
+
+    const handleInputChange = (name, value) => {
+        if (name === "noOfDays" && value > 5) {
+            console.log("Please enter trip days less than 5");
+            return;
+        }
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    useEffect(() => {
+        console.log("Updated formData:", formData);
+    }, [formData]);
+
+    const GenerateTrip = () => {
+        if (formData?.noOfDays > 5) {
+            console.log("Please enter trip days less than 5");
+            return;
+        }
+    }
+
     return (
-        <div className="sm-px md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
-            {/* To create a trip we need to enter some details */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="px-5 mt-10 sm:px-10 md:px-32 lg:px-56 xl:px-72">
+            <div>
+                <h2 className='text-3xl font-bold'>Tell us your travel preferences 🏕️🌴</h2>
+                <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences</p>
+            </div>
+
+            <div className='mt-10 flex flex-col gap-10'>
                 <div>
-                    <h2 className='text-3xl font-bold'>Tell us your travel preferences</h2>
-                    <p className='text-gray-500'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences</p>
+                    <h2 className='text-xl my-3 font-medium'>What is destination of choice? 🏖️</h2>
+
+                    {/* Geoapify Geocoder Autocomplete */}
+                    <GeoapifyContext apiKey={import.meta.env.VITE_GEOAPIFY_API_KEY}>
+                        <GeoapifyGeocoderAutocomplete
+                            placeholder="Search for a city (Ex. Las Vegas)"
+                            type="city" // This limits results to cities only
+                            placeSelect={(value) => {
+                                setPlace(value);
+                                console.log("Selected Place:", value);
+                            }}
+                        />
+                    </GeoapifyContext>
                 </div>
+            </div>
+
+            {/* Number of days */}
+            <div className='mt-10'>
+                <h2 className='text-xl my-3 font-medium'>How many days are you planning your trip? 🗓️</h2>
+                <Input type="number" placeholder="Number of days" min="1" max="5"
+                    onChange={(e) => handleInputChange('noOfDays', e.target.value)} />
+            </div>
+
+            {/* Budget */}
+            <div className='mt-10'>
+                <h2 className='text-xl my-3 font-medium'>What is your budget? 💳</h2>
+                <div className='grid grid-cols-1 sm:grid-cols-3 gap-5 mt-5'>
+                    {BudgetOptions.map((item, index) => (
+                        <div key={index} onClick={() => handleInputChange('budget', item.title)}
+                            className={`p-4 border rounded-lg cursor-pointer transition-all 
+                            hover:shadow-lg hover:border-black/30 hover:-translate-y-1 bg-card 
+                            dark:hover:border-white/30 text-center ${formData.budget == item.title ? 'shadow-lg border-black/30' : ''}`}>
+                            <span className='text-2xl mb-1 inline-block'>{item.icon}</span>
+                            <h3 className='font-bold text-sm text-card-foreground'>{item.title}</h3>
+                            <p className='text-xs text-muted-foreground mt-1'>{item.desc}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* Traveler */}
+            <div className='mt-10'>
+                <h2 className='text-xl my-3 font-medium'>Who do you plan on travelling with on your next adventure? 🥂</h2>
+                <div className='grid grid-cols-1 sm:grid-cols-3 gap-5 mt-5'>
+                    {TravelOptions.map((item, index) => (
+                        <div key={index} onClick={() => handleInputChange('traveler', item.people)}
+                            className={`p-4 border rounded-lg cursor-pointer transition-all 
+                            hover:shadow-lg hover:border-black/30 hover:-translate-y-1 
+                            bg-card dark:hover:border-white/30 text-center ${formData.traveler == item.people ? 'shadow-lg border-black/30' : ''}`}>
+                            <span className='text-2xl mb-1 inline-block'>{item.icon}</span>
+                            <h3 className='font-bold text-sm text-card-foreground'>{item.title}</h3>
+                            <p className='text-xs text-muted-foreground mt-1'>{item.desc}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* Generate Trip */}
+            <div className='my-20 flex justify-end'>
+                <Button onClick={GenerateTrip} >Generate Trip</Button>
             </div>
         </div>
     )
 }
 
 export default CreateTrip
+
+
+
