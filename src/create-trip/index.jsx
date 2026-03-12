26df409ebffe17b2c21d89ecbae1fb8c5a22a490
Niveditha02 <niveditha.mpn@gmail.com>
2026-03-11 20:43:14 -0400

    sonner toast message in index.jsx

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -6,6 +6,7 @@ import '@geoapify/geocoder-autocomplete/styles/minimal.css';
 import { Input } from '@/components/ui/input';
 import { BudgetOptions, TravelOptions } from '@/constants/options';
 import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
 
 function CreateTrip() {
     const [place, setPlace] = useState();
@@ -27,17 +28,19 @@ function CreateTrip() {
     }, [formData]);
 
     const GenerateTrip = () => {
-        if (formData?.noOfDays > 5) {
-            console.log("Please enter trip days less than 5");
+        if (formData?.noOfDays > 5 && !formData?.Location || !formData?.budget || !formData?.traveler) {
+            toast("Please fill all the fields")
             return;
         }
+        console.log(formData);
     }
 
     return (
         <div className="px-5 mt-10 sm:px-10 md:px-32 lg:px-56 xl:px-72">
             <div>
                 <h2 className='text-3xl font-bold'>Tell us your travel preferences 🏕️🌴</h2>
-                <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences</p>
+                <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information,
+                    and our trip planner will generate a customized itinerary based on your preferences</p>
             </div>
 
             <div className='mt-10 flex flex-col gap-10'>
@@ -73,7 +76,8 @@ function CreateTrip() {
                         <div key={index} onClick={() => handleInputChange('budget', item.title)}
                             className={`p-4 border rounded-lg cursor-pointer transition-all 
                             hover:shadow-lg hover:border-black/30 hover:-translate-y-1 bg-card 
-                            dark:hover:border-white/30 text-center ${formData.budget == item.title ? 'shadow-lg border-black/30' : ''}`}>
+                            dark:hover:border-white/30 text-center ${formData.budget == item.title ?
+                                    'shadow-lg border-black/30' : ''}`}>
                             <span className='text-2xl mb-1 inline-block'>{item.icon}</span>
                             <h3 className='font-bold text-sm text-card-foreground'>{item.title}</h3>
                             <p className='text-xs text-muted-foreground mt-1'>{item.desc}</p>
@@ -84,13 +88,15 @@ function CreateTrip() {
 
             {/* Traveler */}
             <div className='mt-10'>
-                <h2 className='text-xl my-3 font-medium'>Who do you plan on travelling with on your next adventure? 🥂</h2>
+                <h2 className='text-xl my-3 font-medium'>Who do you plan on travelling with on
+                    your next adventure? 🥂</h2>
                 <div className='grid grid-cols-1 sm:grid-cols-3 gap-5 mt-5'>
                     {TravelOptions.map((item, index) => (
                         <div key={index} onClick={() => handleInputChange('traveler', item.people)}
                             className={`p-4 border rounded-lg cursor-pointer transition-all 
                             hover:shadow-lg hover:border-black/30 hover:-translate-y-1 
-                            bg-card dark:hover:border-white/30 text-center ${formData.traveler == item.people ? 'shadow-lg border-black/30' : ''}`}>
+                            bg-card dark:hover:border-white/30 text-center 
+                            ${formData.traveler == item.people ? 'shadow-lg border-black/30' : ''}`}>
                             <span className='text-2xl mb-1 inline-block'>{item.icon}</span>
                             <h3 className='font-bold text-sm text-card-foreground'>{item.title}</h3>
                             <p className='text-xs text-muted-foreground mt-1'>{item.desc}</p>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import CreateTrip from './create-trip';
 import Header from './components/custom/Header';
+import { Toaster } from '@/components/ui/sonner';
 
 
 const router = createBrowserRouter([
@@ -21,6 +22,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Toaster />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
